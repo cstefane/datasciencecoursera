60797485b2a42c6d87086e75ac3046ba226b99a7
Cosmin Stefanescu <stefanescu.cosmin@gmail.com>
2015-08-14 21:33:04 +0200

    update

--- corr.R
+++ corr.R
@@ -39,6 +39,7 @@ corr <- function(directory, threshold = 0) {
         
         ## Return a numeric vector of correlations
         ## NOTE: Do not round the result!
-        if (length(vector) == 0) vector <- c(vector, 0)
+        if (length(vector) == 0) vector <- vector(mode="numeric", length=0)
+       
         vector
 }
